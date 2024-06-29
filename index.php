c38679a2e84e22bb7e636086c20d41c982ec21a1
alexandre-82 <44838196+alexandre-82@users.noreply.github.com>
2024-06-29 10:12:17 -0300

    Pagina inicial do teste

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deve pela Net</title>
+</head>
+<body>
+    <h1>DESENVOLVIMENTO PELA INTERNET</h1>
+    <h2>Introdução</h2>
+    <p>Este é um teste, para saber se posso me adaptar a Desenvolver diretamente pelo Navegador sem Precisar instalar qual aplicativo de desenvolvimento.</p>
+</body>
+</html>
